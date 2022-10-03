3fea7de9e222c199103f1072d8856307a958b613
brt9 <50088462+brt9@users.noreply.github.com>
2022-10-03 18:30:39 -0300

    master

--- app/model/lista_estoque/pedido.class.php
+++ app/model/lista_estoque/pedido.class.php
@@ -12,11 +12,15 @@ use Adianti\Database\TRecord;
  * @copyright  Copyright (c) 2021 Barata
  * @license    http://www.adianti.com.br/framework-license
  */
-class cadastro extends TRecord
+class pedido extends TRecord
 {
-    const TABLENAME = 'estoque_gms';
-    const PRIMARYKEY = 'id_item';
-    const IDPOLICY =  'max'; // {max, serial}
+    const TABLENAME = 'pedido_material';
+    const PRIMARYKEY = 'id_pedido_material';
+    const IDPOLICY = 'max'; // {max, serial}
+    
+    CONST CREATEDAT = 'created_at';
+    CONST UPDATEDAT = 'updated_at';
+    CONST DELETEDAT = 'deleted_at';
 
     /**
      * Constructor method
@@ -24,9 +28,11 @@ class cadastro extends TRecord
     public function __construct($id = NULL, $callObjectLoad = TRUE)
     {
         parent::__construct($id, $callObjectLoad);
+        parent::addAttribute('id_pedido_material');
         parent::addAttribute('id_item');
-        parent::addAttribute('descricao');
-        parent::addAttribute('quantidade_estoque');
-        parent::addAttribute('data_cadastro_item');
+        parent::addAttribute('quantidade');
+        parent::addAttribute('id_usuario');
+        parent::addAttribute('id_admin');
     }
+    
 }
